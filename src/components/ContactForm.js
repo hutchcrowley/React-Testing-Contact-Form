1a4e23b6872ba7f6c8b24b7663bce35226064335
Hutch Crowley <tothecoda@gmail.com>
2020-08-02 21:18:18 -0500

    Update finish bulding form. Unit Tests need

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,52 +1,81 @@
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import axios from 'axios'
 
 const ContactForm = () => {
-	const [ data, setData ] = useState()
+	const [ data, setData ] = useState({})
+
+	const [ response, setResponse ] = useState(null)
+
 	const { register, errors, handleSubmit, reset } = useForm({
 		mode: 'onBlur',
 	})
-	const onSubmit = data => {
-		setData(data)
-		register(data)
-	}
 
-	const logger = e => {
-		let data = e.target
-		console.log(`data in ContactForm: ${data}`)
+	const onSubmit = values => {
+		setData(values)
+		axios
+			.post('https://reqres.in/api/users', data)
+			.then(console.log(`data in useEffect: ${data}`))
+			.then(res => {
+				setResponse(res.data)
+			})
+			.catch(err => {
+				setResponse(err.message)
+			})
 	}
 
 	return (
 		<div className='App'>
-			<form
-				className='contact-form'
-				data-testid='contactForm'
-				onChange={logger}
-				onSubmit={handleSubmit(onSubmit)}
-			>
+			<form className='contact-form' data-testid='contactForm' onSubmit={handleSubmit(onSubmit)}>
 				<div data-testid='FNInput'>
 					<label htmlFor='firstName'>First Name*</label>
-					<input name='firstName' placeholder='First Name' ref={register({ required: true })} />
-					{errors.firstName && <p>Looks like there was an error. Error Type: {errors.firstName.type}</p>}
+					<input
+						name='firstName'
+						placeholder='First Name'
+						ref={register({
+							required: true,
+							minLength: { value: 4, message: 'ERROR: first name shoud be at least 4 chars.' },
+						})}
+					/>
+					{errors.firstName && <p> {errors.firstName.message}</p>}
 				</div>
 				<div data-testid='lnInput'>
 					<label htmlFor='lastName'>Last Name*</label>
-					<input name='lastName' placeholder='Last Name' ref={register({ required: true })} />
-					{errors.lastName && <p>Looks like there was an error. Error Type: {errors.lastName.type}</p>}
+					<input
+						name='lastName'
+						placeholder='Last Name'
+						ref={register({
+							required: true,
+							minLength: { value: 4, message: 'ERROR: last name should be at least 4 chars.' },
+						})}
+					/>
+					{errors.lastName && <p>{errors.lastName.message}</p>}
 				</div>
-				<div data-testid='eInput'>
+				<div data-testid='emailInput'>
 					<label htmlFor='email'>Email*</label>
-					<input name='email' placeholder='email@somesite.com' ref={register({ required: true })} />
-					{errors.email && <p>Looks like there was an error. Error Type: {errors.email.type}</p>}
+					<input
+						name='email'
+						placeholder='email@somesite.com'
+						type='email'
+						ref={register({
+							required: true,
+							minLength: {
+								value: 15,
+								message: 'Entered value does not match email pattern',
+							},
+						})}
+					/>
+					{errors.email && <p>{errors.email.message}</p>}
 				</div>
 				<div data-testid='mInput'>
 					<label htmlFor='message'>Message</label>
 					<textarea name='message' ref={register({ required: false })} />
 				</div>
-				{data && (
+				{response && (
 					<pre className='data-display'>
-						<h3>Here is the submitted data:</h3>
 						{JSON.stringify(data, null, 2)}
+						<h3>Here is the submitted data:</h3>
+						{JSON.stringify(response, null, 2)}
 					</pre>
 				)}
 				<button type='submit' data-testid='sBtn' className='btn'>
